6fb83d93ba1b57f0041843cb4e8234c82dc1d911
zackozakoi <kurotina9600@gmail.com>
2024-12-21 17:23:12 +0800

    员工启用禁用业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -23,4 +23,9 @@ public interface EmployeeMapper {
     void insert(Employee employee);
 
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 根据id修改员工信息
+     */
+    void update(Employee employee);
 }
